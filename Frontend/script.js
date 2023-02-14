ad534dfe77374ba7405e4a6d307ab462588b9df5
Joey <Joeyew2014@gmail.com>
2023-02-14 18:25:43 -0500

    Fixed burger menu and some text

--- Frontend/script.js
+++ Frontend/script.js
@@ -1,12 +1,19 @@
-const bus = document.getElementById("bus");
+// const bus = document.getElementById("bus");
 
-let x = 0;
-let y = 0;
-let speed = 5;
+// let x = 0;
+// let y = 0;
+// let speed = 5;
 
-setInterval(() => {
-  x += speed;
-  y += speed;
-  bus.style.left = x + "px";
-  bus.style.top = y + "px";
-}, 50);
+// setInterval(() => {
+//   x += speed;
+//   y += speed;
+//   bus.style.left = x + "px";
+//   bus.style.top = y + "px";
+// }, 50);
+
+const openMenu = () => {
+  document.body.classList += " menu--open"
+ }
+const closeMenu = () =>{
+ document.body.classList.remove('menu--open')
+ }
